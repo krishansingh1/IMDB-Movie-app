ad739c5f5aada064b9b773bf61af7babc79551e0
Krishan Kumar <kkrishan7074@gmail.com>
2023-07-06 12:15:30 +0530

    Commit

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const buttons = document.querySelectorAll("[data-carousel-button]");
 const slide = document.querySelector(".slide");
+
+buttons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        console.log(button);
