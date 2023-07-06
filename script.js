a95cceb6a87bf5b03ad350fc9716198764d82a55
Krishan Kumar <kkrishan7074@gmail.com>
2023-07-06 12:23:53 +0530

    Commit

--- script.js
+++ script.js
@@ -11,5 +11,9 @@ buttons.forEach((button) => {
         // console.log(activeSlide);
         let newIndex = [...slides.children].indexOf(activeSlide) + offSet;
         console.log(newIndex);
-        if(newIndex < 0 ) newIndex = slides.children.length - 1;
-        if(newIndex >= slides.children.length) newIndex = 0;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    })
+})
