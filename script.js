ee243a734bd82906df9efdc99d53eff8bc49e024
Krishan Kumar <kkrishan7074@gmail.com>
2023-07-06 12:15:45 +0530

    Commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const buttons = document.querySelectorAll("[data-carousel-button]");
 const slide = document.querySelector(".slide");
 
-buttons.forEach((button)=>{
-    button.addEventListener("click",()=>{
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
         console.log(button);
+        const offSet = button.dataset.carouselButton === "next" ? 1 : -1;
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+        // console.log(slides.children);
